68dde061554411e36c9f14fccd7de5bf4d6d189d
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-15 09:55:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,9 +8,11 @@ var playlist = {
 function updatePlaylist(playlist, key, value)
 {
   playlist[key] = value
+  return playlist
 }
 
 function removeFromPlaylist(playlist, key)
 {
   delete playlist.key
+  return playlist
 }
